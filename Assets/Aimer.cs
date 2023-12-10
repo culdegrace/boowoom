e7ba12c9fa00c29636e3d6310442e4d59456b1be
cul de grace <culdegracegames@proton.me>
2023-12-10 15:45:01 -0800

    Account for intersection outside of stage.

--- Assets/Aimer.cs
+++ Assets/Aimer.cs
@@ -21,28 +21,47 @@ public class Aimer : MonoBehaviour
     {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
+
+        // Intersect either with the stage OR with this infinite plane
+        var groundPlane = new Plane(Vector3.up, -0.1f);
+        float enter = 0.0f;
+
+        bool found = false;
+        var lookAtPosition = Vector3.zero;
+        // Mouse intersects with stage.
         if (Physics.Raycast(ray, out hit))
         {
             if(hit.transform.name == "STAGE")
             {
-                // Mouse intersects with stage.
-                var lookAtPosition = hit.point;
-                // Swiveling the hinge.
-                var hingeOffs = Quaternion.LookRotation(lookAtPosition - hingeTransform.position, Vector3.up);
-                var hingeDefault = hingeTransform.eulerAngles;
-                var hingeRotation = Quaternion.Euler(hingeDefault.x, hingeOffs.eulerAngles.y, hingeDefault.z);
-                var hingeLerp = Quaternion.Lerp(hingeTransform.rotation, hingeRotation, 0.1f);
-                hingeTransform.rotation = hingeLerp;
-
-                // Rotating the barrel.
-                var barrelOffs = Quaternion.LookRotation(lookAtPosition - barrelTransform.position, Vector3.up);
-                var barrelDefault = barrelTransform.eulerAngles;
-                var barrelRotation = Quaternion.Euler(barrelOffs.eulerAngles.x - 90, barrelOffs.eulerAngles.y, barrelOffs.eulerAngles.z);
-                var barrelLerp = Quaternion.Lerp(barrelTransform.rotation, barrelRotation, 0.1f);
-                barrelTransform.rotation = barrelLerp;
-                
-
+                found = true;
+                lookAtPosition = hit.point;
             }
         }
+        // Mouse intersects with area outside of the stage (groundPlane above)
+        else if (groundPlane.Raycast(ray, out enter))
+        {
+            found = true;
+            lookAtPosition = ray.GetPoint(enter);
+        }
+
+        if (!found)
+        {
+            return;
+        }
+
+        // Swiveling the hinge.
+        var hingeOffs = Quaternion.LookRotation(lookAtPosition - hingeTransform.position, Vector3.up);
+        var hingeDefault = hingeTransform.eulerAngles;
+        var hingeRotation = Quaternion.Euler(hingeDefault.x, hingeOffs.eulerAngles.y, hingeDefault.z);
+        var hingeLerp = Quaternion.Lerp(hingeTransform.rotation, hingeRotation, 0.1f);
+        hingeTransform.rotation = hingeLerp;
+
+        // Rotating the barrel.
+        var barrelOffs = Quaternion.LookRotation(lookAtPosition - barrelTransform.position, Vector3.up);
+        var barrelDefault = barrelTransform.eulerAngles;
+        var barrelRotation = Quaternion.Euler(barrelOffs.eulerAngles.x - 90, barrelOffs.eulerAngles.y, barrelOffs.eulerAngles.z);
+        var barrelLerp = Quaternion.Lerp(barrelTransform.rotation, barrelRotation, 0.1f);
+        barrelTransform.rotation = barrelLerp;
+
     }
 }
