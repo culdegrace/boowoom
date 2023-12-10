db947affbb061d5e81bc5ff4827cf97a19f3f61d
cul de grace <culdegracegames@proton.me>
2023-12-10 15:22:21 -0800

    Rotate player to track mouse.

--- Assets/Aimer.cs
+++ Assets/Aimer.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -11,15 +12,37 @@ public class Aimer : MonoBehaviour
     void Start()
     {
         baseTransform = transform.Find("BASE");
-        hingeTransform = baseTransform.Find("HINGE");
-        barrelTransform = hingeTransform.Find("BARREL");
+        hingeTransform = transform.Find("HINGE");
+        barrelTransform = transform.Find("BARREL");
     }
 
     // Update is called once per frame
     void Update()
     {
-        float rotationSpeed = 45.0f;
-        float step = rotationSpeed * Time.deltaTime;
-        baseTransform.Rotate(0, 0, step);
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            if(hit.transform.name == "STAGE")
+            {
+                // Mouse intersects with stage.
+                var lookAtPosition = hit.point;
+                // Swiveling the hinge.
+                var hingeOffs = Quaternion.LookRotation(lookAtPosition - hingeTransform.position, Vector3.up);
+                var hingeDefault = hingeTransform.eulerAngles;
+                var hingeRotation = Quaternion.Euler(hingeDefault.x, hingeOffs.eulerAngles.y, hingeDefault.z);
+                var hingeLerp = Quaternion.Lerp(hingeTransform.rotation, hingeRotation, 0.1f);
+                hingeTransform.rotation = hingeLerp;
+
+                // Rotating the barrel.
+                var barrelOffs = Quaternion.LookRotation(lookAtPosition - barrelTransform.position, Vector3.up);
+                var barrelDefault = barrelTransform.eulerAngles;
+                var barrelRotation = Quaternion.Euler(barrelOffs.eulerAngles.x - 90, barrelOffs.eulerAngles.y, barrelOffs.eulerAngles.z);
+                var barrelLerp = Quaternion.Lerp(barrelTransform.rotation, barrelRotation, 0.1f);
+                barrelTransform.rotation = barrelLerp;
+                
+
+            }
+        }
     }
 }
