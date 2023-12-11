ce6e568c4f3f5619f5e2861f2bc44be8e79538bf
cul de grace <culdegracegames@proton.me>
2023-12-10 20:45:11 -0800

    Get rocket direction.

--- Assets/Aimer.cs
+++ Assets/Aimer.cs
@@ -19,6 +19,7 @@ public class Aimer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+    // ROTATE TWD MOUSE
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
 
@@ -52,16 +53,23 @@ public class Aimer : MonoBehaviour
         // Swiveling the hinge.
         var hingeOffs = Quaternion.LookRotation(lookAtPosition - hingeTransform.position, Vector3.up);
         var hingeDefault = hingeTransform.eulerAngles;
-        var hingeRotation = Quaternion.Euler(hingeDefault.x, hingeOffs.eulerAngles.y, hingeDefault.z);
+        var hingeRotation = Quaternion.Euler(hingeDefault.x, hingeOffs.eulerAngles.y, hingeDefault.z); // Rotate y axis only.
         var hingeLerp = Quaternion.Lerp(hingeTransform.rotation, hingeRotation, 0.1f);
         hingeTransform.rotation = hingeLerp;
 
         // Rotating the barrel.
         var barrelOffs = Quaternion.LookRotation(lookAtPosition - barrelTransform.position, Vector3.up);
-        var barrelDefault = barrelTransform.eulerAngles;
         var barrelRotation = Quaternion.Euler(barrelOffs.eulerAngles.x - 90, barrelOffs.eulerAngles.y, barrelOffs.eulerAngles.z);
         var barrelLerp = Quaternion.Lerp(barrelTransform.rotation, barrelRotation, 0.1f);
         barrelTransform.rotation = barrelLerp;
 
+    // POLL INPUT
+        if (Input.anyKey)
+        {
+            Debug.Log("Drawing ray.");
+            // Direction of barrel.
+            Vector3 directionToFace = lookAtPosition - barrelTransform.position;
+            Debug.DrawRay(barrelTransform.position, directionToFace, Color.red, 2f);
+        }
     }
 }
